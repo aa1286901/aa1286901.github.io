2db78152323083edb9bcc9958a4fb928bd1af15b
Ahmedov <39351267+aa1286901@users.noreply.github.com>
2023-12-04 14:27:12 +0300

    Update routes-1.js

--- apps/js/routes-1.js
+++ apps/js/routes-1.js
@@ -4,8 +4,17 @@ routes = [
       url: './index.php',
     },
     {
+      path: '/app/',
+      url: '',
+    },
+    {
       path: '/category/',
       url: './pages/catApps.php',
+    },
+    // Default route (404 page). MUST BE THE LAST
+    {
+      path: '(.*)',
+      url: '',
     },
   ];
   
