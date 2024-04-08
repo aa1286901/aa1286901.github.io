65bcbebeebeff0035acdd6724bbb4bdf7a93f751
Ahmedov <39351267+aa1286901@users.noreply.github.com>
2024-04-08 14:01:10 +0300

    Update black.js

--- kp/black.js
+++ kp/black.js
@@ -9,9 +9,9 @@
    
 	/*Запуск сторонних плагинов*/
     Lampa.Utils.putScriptAsync([
-          './online.js',
-         // './o.js',
-	  'https://nb557.github.io/plugins/online_mod.js',
+         // './online.js',
+          './o.js',
+	//  'https://nb557.github.io/plugins/online_mod.js',
           './tmdbproxy.js',
           './collections.js', 
           './tracks.js',
