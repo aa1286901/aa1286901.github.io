bbd2ea44f9f2758b3bf819dad8b0ed05174c6b76
Ahmedov <39351267+aa1286901@users.noreply.github.com>
2024-04-09 14:31:52 +0300

    Add files via upload

--- kp/black.js
+++ kp/black.js
@@ -6,6 +6,7 @@
    // localStorage.clear();
     Lampa.Storage.set('parser_use', 'true');
     Lampa.Storage.set('jackett_url', 'jacred.xyz');
+    Lampa.Storage.set('screensaver', 'false');
     // Прокси для Онлайн мод
     Lampa.Storage.set('online_mod_proxy_kodik', 'true');
     Lampa.Storage.set('online_mod_proxy_zetflix', 'true');
@@ -17,6 +18,7 @@
     Lampa.Storage.set('online_mod_proxy_redheadsound', 'true');
     Lampa.Storage.set('online_mod_proxy_rezka', 'true');
     Lampa.Storage.set('online_mod_proxy_cdnmovies', 'true');
+    Lampa.Storage.set('online_mod_save_last_balanser', 'true');
    
 	/*Запуск сторонних плагинов*/
     Lampa.Utils.putScriptAsync([
@@ -43,8 +45,8 @@ Lampa.Settings.listener.follow('open', function (e) {
      $('div[data-component="tmdb"]').remove();
      $('div[data-component="sisi"]').remove();
      $('div[data-component="my_iptv2"]').remove();
-   //  $('div[data-component="parser"]').remove();
-   //  $('div[data-component="server"]').remove();
+     $('div[data-component="parser"]').remove();
+     $('div[data-component="server"]').remove();
      $('div[data-component="parental_control"]').remove();
      $('div[data-component="filmix"]').remove();
    }, 5)
@@ -53,7 +55,7 @@ Lampa.Settings.listener.follow('open', function (e) {
 	/*Удаляем верхние кнопки */
 
    setTimeout(function(){
-    // $('.open--feed').remove();
+     $('.open--feed').remove();
      $('.open--premium').remove();
      $('.open--broadcast').remove();
      $('.head__time').remove();
@@ -96,6 +98,7 @@ Lampa.Settings.listener.follow('open', function (e) {
          $('.view--trailer').insertBefore($('.button--play'));
            $('.view--torrent').insertBefore($('.view--trailer'));
 	 $('.button--play').remove();
+   $('.view--torrent').remove();
 	 $(".view--online_mod", Lampa.Activity.active().activity.render()).empty().append('<svg viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 32 32"><path d="m17 14.5 4.2-4.5L4.9 1.2c-.1-.1-.3-.1-.6-.2L17 14.5zM23 21l5.9-3.2c.7-.4 1.1-1 1.1-1.8s-.4-1.5-1.1-1.8L23 11l-4.7 5 4.7 5zM2.4 1.9c-.3.3-.4.7-.4 1.1v26c0 .4.1.8.4 1.2L15.6 16 2.4 1.9zM17 17.5 4.3 31c.2 0 .4-.1.6-.2L21.2 22 17 17.5z" fill="currentColor" fill="#ffffff" class="fill-000000"></path></svg>&nbsp;&nbsp;Смотреть Онлайн');
           Lampa.Controller.toggle('full_start');
        },10);
@@ -126,16 +129,16 @@ if (!Lampa.Storage.get('player_def')) {
  Lampa.Storage.set('player_def', true);
 }
 
-	/*Добавляем раздел Мультфильмы в главное меню
+	/*Добавляем раздел Мультфильмы в главное меню*/
 
       function multstart() {
            var ico = '<svg fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="256px" height="256px" viewBox="0 0 76.688 76.687" xml:space="preserve" stroke="currentColor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M75.191,30.104h-0.598c-0.574-6.778-4.99-12.47-11.062-14.894C57.993,7.232,48.773,1.992,38.345,1.992 c-10.427,0-19.647,5.237-25.187,13.217c-6.07,2.424-10.485,8.114-11.06,14.895H1.5c-0.828,0-1.5,0.673-1.5,1.5 c0,0.828,0.672,1.5,1.5,1.5h0.598C2.56,38.56,5.502,43.321,9.802,46.233l4.249,27.192c0.114,0.73,0.743,1.271,1.482,1.271h11.248 c0.828,0,1.5-0.672,1.5-1.5c0-5.55,4.515-10.063,10.063-10.063c5.548,0,10.063,4.517,10.063,10.063c0,0.828,0.672,1.5,1.5,1.5 h11.248c0.737,0,1.366-0.539,1.479-1.271l4.25-27.192c4.301-2.914,7.242-7.673,7.703-13.129h0.6c0.826,0,1.5-0.672,1.5-1.5 C76.689,30.777,76.02,30.104,75.191,30.104z M38.345,4.992c8.084,0,15.374,3.49,20.434,9.042c-0.582-0.059-1.172-0.09-1.77-0.09 c-9.229,0-16.816,7.123-17.582,16.157h-2.167c-0.765-9.034-8.352-16.156-17.582-16.156c-0.597,0-1.186,0.03-1.768,0.09 C22.972,8.484,30.259,4.992,38.345,4.992z M65.993,32.527l-1.854,11.874c-2.113,1.185-4.541,1.86-7.129,1.86 c-8.082,0-14.658-6.574-14.658-14.657c0-8.083,6.576-14.657,14.658-14.657c1.598,0,3.131,0.266,4.569,0.738 C64.35,21.97,65.973,27.061,65.993,32.527z M10.699,32.527c0.021-5.466,1.645-10.557,4.413-14.842 c1.439-0.474,2.973-0.738,4.569-0.738c8.083,0,14.658,6.574,14.658,14.657c0,8.082-6.575,14.658-14.658,14.658 c-2.587,0-5.015-0.682-7.127-1.86L10.699,32.527z M5.025,31.604c0-4.497,2.04-8.523,5.237-11.215 c-1.645,3.755-2.563,7.896-2.563,12.252c0,0.077,0.006,0.155,0.018,0.23l1.382,8.846C6.58,39.083,5.025,35.526,5.025,31.604z M59.872,71.696h-8.549c-0.746-6.5-6.281-11.563-12.979-11.563c-6.697,0-12.23,5.063-12.977,11.563h-8.55l-3.705-23.715 c2.032,0.817,4.246,1.28,6.567,1.28c9.23,0,16.817-7.123,17.582-16.158h2.167c0.765,9.035,8.353,16.158,17.582,16.158 c2.322,0,4.535-0.463,6.566-1.28L59.872,71.696z M67.596,41.716l1.381-8.846c0.012-0.075,0.018-0.153,0.018-0.23 c0-4.354-0.918-8.497-2.562-12.252c3.199,2.69,5.24,6.718,5.24,11.215C71.67,35.526,70.111,39.083,67.596,41.716z"></path> <path d="M25.777,25.671c0-3.36-2.734-6.095-6.096-6.095c-3.36,0-6.094,2.732-6.094,6.095c0,3.36,2.733,6.094,6.094,6.094 S25.777,29.032,25.777,25.671z M19.68,28.765c-0.373,0-0.727-0.077-1.058-0.197c1.995-0.157,3.575-1.812,3.575-3.846 c0-0.356-0.064-0.696-0.155-1.024c0.451,0.538,0.733,1.222,0.733,1.977C22.777,27.377,21.387,28.765,19.68,28.765z M19.198,24.721 c0,0.482-0.395,0.876-0.877,0.876c-0.483,0-0.876-0.394-0.876-0.876s0.393-0.877,0.876-0.877 C18.803,23.844,19.198,24.239,19.198,24.721z"></path> <path d="M49.7,25.671c0,3.359,2.731,6.094,6.095,6.094c3.36,0,6.094-2.732,6.094-6.094c0-3.362-2.732-6.095-6.094-6.095 C52.432,19.576,49.7,22.311,49.7,25.671z M53.559,24.721c0-0.482,0.393-0.877,0.877-0.877c0.481,0,0.875,0.395,0.875,0.877 s-0.394,0.876-0.875,0.876C53.95,25.597,53.559,25.203,53.559,24.721z M55.795,28.765c-0.373,0-0.728-0.077-1.06-0.197 c1.996-0.157,3.573-1.812,3.573-3.846c0-0.356-0.063-0.696-0.153-1.025c0.451,0.538,0.731,1.222,0.731,1.977 C58.889,27.377,57.501,28.765,55.795,28.765z"></path> <path d="M46.2,50.151h-8.555c-0.827,0-1.5,0.672-1.5,1.5c0,0.827,0.673,1.5,1.5,1.5H46.2c0.828,0,1.5-0.673,1.5-1.5 C47.7,50.823,47.029,50.151,46.2,50.151z"></path> </g> </g> </g></svg>'
            var mult = $('<li class="menu__item selector" data-action="mult"><div class="menu__ico">' + ico + '</div><div class="menu__text">Мультфильмы</div></li>');
-           mult.on('hover:enter', function() { Lampa.Activity.push({"url":"","title":"Мультфильмы - CUB","component":"category","genres":16,"id":16,"source":"cub","card_type":true,"page":1}) });
+           mult.on('hover:enter', function() { Lampa.Activity.push({"url":"","title":"Мультфильмы - CUB","component":"category","genres":16,"id":16,"source":"tmdb","card_type":true,"page":1}) });
                 $('.menu .menu__list').eq(0).append(mult);
                   setTimeout(function() {$("[data-action=mult]").insertBefore($("[data-action=catalog]"));}, 2000)
            }
-  setTimeout(function() { multstart() },2000)*/
+  setTimeout(function() { multstart() },2000)
    
       /*Start FIX*/
     
