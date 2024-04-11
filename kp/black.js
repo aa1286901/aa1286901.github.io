ef551cc7ad883c01fc1dbb3a57de97768b7e74ef
Ahmedov <39351267+aa1286901@users.noreply.github.com>
2024-04-11 10:12:51 +0300

    Add files via upload

--- kp/black.js
+++ kp/black.js
@@ -23,7 +23,7 @@
    
 	/*Запуск сторонних плагинов*/
     Lampa.Utils.putScriptAsync([
-          './online.js',
+          './o.js',
           './tmdbproxy.js',
           './collections.js', 
           './tracks.js',
