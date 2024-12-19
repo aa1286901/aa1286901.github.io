1dab63262f9e2d8d3d02c7c45cc246fbf6f43e9d
Ahmedov <39351267+aa1286901@users.noreply.github.com>
2024-12-19 10:32:50 +0300

    Add files via upload

--- arab/script.js
+++ arab/script.js
@@ -116,7 +116,7 @@ window.onload = () => {
 };
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
         .then(() => console.log('Service Worker зарегистрирован'))
         .catch((error) => console.error('Ошибка регистрации Service Worker:', error));
 }
