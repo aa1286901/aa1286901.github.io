71213e19dfe73041f6ba1219154f03ad3fae07ee
Ahmedov <39351267+aa1286901@users.noreply.github.com>
2025-02-02 03:06:43 +0300

    Add files via upload

---
+++ app_store/js/apps.js
@@ -0,0 +1,11 @@
+const apps = [
+    {
+        "name": "Сбербанк (Бюджет Онлайн)",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/58/98/d3/5898d34e-9965-49ac-acca-b806bf579806/AppIcon-0-0-1x_U007ephone-0-85-220.png/114x114bb.jpg",
+        "link": "itms-services://?action=download-manifest&url=plist/com.budgetonline.ios.plist",
+        "description": "v.16.0.1 | 496 Mb",
+        "category": "#All"
+    },
+
+
+]
---
+++ app_store/js/script.js
@@ -0,0 +1,44 @@
+// Получение элементов
+const appList = document.getElementById("app-list");
+const categories = document.querySelectorAll(".category");
+
+// Функция для отображения приложений
+function renderApps(filter = "#All") {
+  appList.innerHTML = ""; // Очистка списка перед отрисовкой
+
+  const filteredApps = apps.filter((app) => app.category === filter);
+
+  filteredApps.forEach((app) => {
+    const appCard = document.createElement("div");
+    appCard.classList.add("app-card");
+
+    appCard.innerHTML = `
+      <img src="${app.image}" alt="${app.name} Icon" class="app-icon">
+      <div class="app-info">
+        <h2>${app.name}</h2>
+        <p>${app.description}</p>
+      </div>
+      <a href="${app.link}" class="download-button" target="_blank">
+        <i class="fas fa-cloud-download-alt"></i>
+      </a>
+    `;
+
+    appList.appendChild(appCard);
+  });
+}
+
+// Слушатели для категорий
+categories.forEach((category) => {
+  category.addEventListener("click", () => {
+    // Удаляем активный класс у всех категорий
+    categories.forEach((cat) => cat.classList.remove("active"));
+    category.classList.add("active");
+
+    // Получаем категорию и отображаем
+    const filter = category.getAttribute("data-category");
+    renderApps(filter);
+  });
+});
+
+// Первоначальная отрисовка
+renderApps();
