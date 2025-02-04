877b4dc145bf3c01755751e2ba1c45d008c24d8a
Ahmedov <39351267+aa1286901@users.noreply.github.com>
2025-02-04 19:19:08 +0300

    Update apps.js

--- app_store/js/apps.js
+++ app_store/js/apps.js
@@ -5,6 +5,12 @@ const apps = [
         "link": "itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/aa1286901/aa1286901.github.io/refs/heads/master/app_store/plist/com.budgetonline.ios.plist",
         "description": "v.16.0.1 | 496 Mb",
         "category": "#All"
+    },    {
+        "name": "ChatGPT (OpenAI)",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/0b/47/24/0b4724ef-343c-b7bc-7aec-48d35079d624/AppIcon-0-0-1x_U007epad-0-1-85-220.png/512x512bb.png",
+        "link": "itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/aa1286901/aa1286901.github.io/refs/heads/master/app_store/plist/com.openai.chat.plist",
+        "description": "v.1.2025.031 | 64 Mb",
+        "category": "#All"
     },
 
 
