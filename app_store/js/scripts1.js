d166979437b5dbb2da1547589076f42c0dd68f86
Ahmedov <39351267+aa1286901@users.noreply.github.com>
2025-02-21 09:42:34 +0300

    Update scripts1.js

--- app_store/js/scripts1.js
+++ app_store/js/scripts1.js
@@ -36,12 +36,13 @@ function renderApps(filter = "#Social") {
   filteredApps.forEach((app) => {
     const appCard = document.createElement("div");
     appCard.classList.add("app-card");
-
+    const appCategories = app.appName.split("|").map((s) => s.trim()); 
+    
     appCard.innerHTML = `
    <div class="app-header">
       <img src="${app.appImage}" alt="${app.appName}" class="app-icon">
       <div class="app-info">
-        <h2>${categories[0]}</h2> 
+        <h2>${appCategories[0]}</h2>
         <small>Версия: ${app.appVersion}</small>
       </div>
    </div>
